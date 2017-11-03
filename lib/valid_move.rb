759f81459bd4756dd4180e65979054325e4d998f
Crista Cooper <aine3d@gmail.com>
2017-11-03 20:29:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if (index.between?(0, 8))
     return true
-  elsif position_taken? == true
+  elsif position_taken?(board, index) == true
     return false
   else index.between?(0, 8) == false
     return false
